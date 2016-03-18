9f5868f1d2622691ba4ff16d5603c885f8e7092b
Marieglahn <marieglahn@hotmail.com>
2016-03-18 11:39:52 +0100

    added more charts

--- dist/js/charts.js
+++ dist/js/charts.js
@@ -2,12 +2,18 @@
 'use strict';
 
 
-function print_filter(filter){
-	var f=eval(filter);
-	if (typeof(f.length) != "undefined") {}else{}
-	if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
-	if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
-	console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
+function print_filter(filter) {
+    var f = eval(filter);
+    if (typeof (f.length) != "undefined") {} else {}
+    if (typeof (f.top) != "undefined") {
+        f = f.top(Infinity);
+    } else {}
+    if (typeof (f.dimension) != "undefined") {
+        f = f.dimension(function (d) {
+            return "";
+        }).top(Infinity);
+    } else {}
+    console.log(filter + "(" + f.length + ") = " + JSON.stringify(f).replace("[", "[\n\t").replace(/}\,/g, "},\n\t").replace("]", "\n]"));
 }
 
 /* jshint globalstrict: true */
@@ -18,6 +24,10 @@ function print_filter(filter){
 var nVolumeChart = dc.barChart('#n-volume-chart');
 var quarterChart = dc.pieChart('#quarter-chart');
 var containerTypesChart = dc.pieChart('#container-types-chart');
+var shipmentTypesChart = dc.pieChart('#shipment-types-chart');
+var dayOfWeekChart = dc.rowChart('#day-of-week-chart');
+var originServicesChart = dc.rowChart('#originservices-chart');
+var destinationServicesChart = dc.rowChart('#destinationservices-chart');
 
 //### Load your data
 
@@ -33,12 +43,12 @@ d3.csv('../dist/BATBI-N-changed.csv', function (data) {
     // Since its a csv file we need to format the data a bit.
     var dateFormat = d3.time.format('%Y-%m-%d');
     var numberFormat = d3.format('.2f');
-    
+
     data.forEach(function (d) {
-           d.dd = dateFormat.parse(d.ETD);
-           d.month = d3.time.month(d.dd); // pre-calculate month for better performance
-            d.close = +d.close; // coerce to number
-            d.open = +d.open;
+        d.dd = dateFormat.parse(d.ETD);
+        d.month = d3.time.month(d.dd); // pre-calculate month for better performance
+        d.close = +d.close; // coerce to number
+        d.open = +d.open;
     });
 
     //### Create Crossfilter Dimensions and Groups
@@ -96,81 +106,33 @@ d3.csv('../dist/BATBI-N-changed.csv', function (data) {
         return d.dd;
     });
 
+
+    var minDate = dateDimension.bottom(1)[0].dd;
+    var maxDate = dateDimension.top(1)[0].dd;
+
     // Dimension by month
     var moveMonths = nikeData.dimension(function (d) {
         return d.month;
     });
-    
+
     // Group by total movement within month
     var monthlyMoveGroup = moveMonths.group().reduceSum(function (d) {
         return Math.abs(d.close - d.open);
     });
     // Group by total volume within move, and scale down result
-//    var volumeByMonthGroup = moveMonths.group().reduceSum(function (d) {
-//        return d.Measurement / 500000;
-//    });
-    
+    //    var volumeByMonthGroup = moveMonths.group().reduceSum(function (d) {
+    //        return d.Measurement / 500000;
+    //    });
+
     var volumeByMonthGroup = moveMonths.group().reduceSum(function (d) {
         return d.Measurement;
     });
-    
-        //ContainerTypes
-//    var containerTypes  = nikeData.dimension(function(d) {return d.ContainerType;});
-//    var containerDRY_filter = containerTypes.filter("DRY");
-//    var containerTRAI_filter = containerTypes.filter("TRAI");
-//    var containerHIGH_filter = containerTypes.filter("HIGH");
-//    var containerAIR_filter = containerTypes.filter("AIR");
-//    containerTypes.filterAll();
-//
-////    //ShipmentType
-//    var shipmentTypes = nikeData.dimension(function(d) {return d.ShipmentType;});
-//    var shipmentOCE_filter = containerTypes.filter("OCE");
-//    var shipmentIEL_filter = containerTypes.filter("IEL");
-//    var shipmentAIR_filter = containerTypes.filter("AIR");
-//    shipmentTypes.filterAll();
-//    
-////    //OriginServices
-//    var originServices  = nikeData.dimension(function(d) {return d.OriginService;});
-//    var originServiceCFS_filter = originServices.filter("CFS");
-//    var originServiceCY_filter = originServices.filter("CY");
-//    originServices.filterAll();
-//    
-////    //DestinationServices
-//    var destServices  = nikeData.dimension(function(d) {return d.DestinationService;});
-//    var destServiceCFS_filter = destServices.filter("CFS");
-//    var destServiceCY_filter = destServices.filter("CY");
-//    destServices.filterAll();
-    
-//    var indexAvgByMonthGroup = moveMonths.group().reduce(
-//        function (p, v) {
-//            ++p.days;
-//            p.total += (v.open + v.close) / 2;
-//            p.avg = Math.round(p.total / p.days);
-//            return p;
-//        },
-//        function (p, v) {
-//            --p.days;
-//            p.total -= (v.open + v.close) / 2;
-//            p.avg = p.days ? Math.round(p.total / p.days) : 0;
-//            return p;
-//        },
-//        function () {
-//            return {days: 0, total: 0, avg: 0};
-//        }
-//    );
-
-    // Create categorical dimension
-//    var gainOrLoss = nikeData.dimension(function (d) {
-//        return d.open > d.close ? 'Loss' : 'Gain';
-//    });
-    // Produce counts records in the dimension
-//    var gainOrLossGroup = gainOrLoss.group();
 
     // Determine a histogram of percent changes
-//    var fluctuation = nikeData.dimension(function (d) {
-//        return Math.round((d.close - d.open) / d.open * 100);
-//    });
-//    var fluctuationGroup = fluctuation.group();
+    //    var fluctuation = nikeData.dimension(function (d) {
+    //        return Math.round((d.close - d.open) / d.open * 100);
+    //    });
+    //    var fluctuationGroup = fluctuation.group();
 
     // Summarize volume by quarter
     var quarter = nikeData.dimension(function (d) {
@@ -185,52 +147,31 @@ d3.csv('../dist/BATBI-N-changed.csv', function (data) {
             return 'Q4';
         }
     });
-    
+
     var quarterGroup = quarter.group().reduceSum(function (d) {
         return d.Measurement;
     });
-    
-    
-    
-    
-    
-    
-    // Summarize volume by quarter
-//    var containerTypes = nikeData.dimension(function (d) {
-         //ContainerTypes
-    var containerTypes  = nikeData.dimension(function(d) {return d.ContainerType;});
-//    var containerDRY_filter = containerTypes.filter("DRY");
-//    var containerTRAI_filter = containerTypes.filter("TRAI");
-//    var containerHIGH_filter = containerTypes.filter("HIGH");
-//    var containerAIR_filter = containerTypes.filter("AIR");
-//    containerTypes.filterAll();
-//        
-//        
-//        
-//        var month = d.ContainerType;
-//        if (month <= 2) {
-//            return 'Q1';
-//        } else if (month > 2 && month <= 5) {
-//            return 'Q2';
-//        } else if (month > 5 && month <= 8) {
-//            return 'Q3';
-//        } else {
-//            return 'Q4';
-//        }
-//    });
-    
-//    // Create categorical dimension
-//    var gainOrLoss = ndx.dimension(function (d) {
-//        return d.open > d.close ? 'Loss' : 'Gain';
-//    });
-    // Produce counts records in the dimension
+
+    var containerTypes = nikeData.dimension(function (d) {
+        return d.ContainerType;
+    });
     var containerTypesGroup = containerTypes.group();
 
-    
-    
-    
-    
-    
+    var shipmentTypes = nikeData.dimension(function (d) {
+        return d.ShipmentType;
+    });
+    var shipmentTypesGroup = shipmentTypes.group();
+
+    var originServices = nikeData.dimension(function (d) {
+        return d.OriginService;
+    });
+    var originServicesGroup = originServices.group();
+
+    var destinationServices = nikeData.dimension(function (d) {
+        return d.DestinationService;
+    });
+    var destinationServicesGroup = destinationServices.group();
+
     // Counts per weekday
     var dayOfWeek = nikeData.dimension(function (d) {
         var day = d.dd.getDay();
@@ -239,14 +180,19 @@ d3.csv('../dist/BATBI-N-changed.csv', function (data) {
     });
     var dayOfWeekGroup = dayOfWeek.group();
 
-    nVolumeChart.width(990) /* dc.barChart('#monthly-volume-chart', 'chartGroup'); */
+    nVolumeChart.width(990)
         .height(140)
-        .margins({top: 0, right: 50, bottom: 20, left: 40})
+        .margins({
+            top: 0,
+            right: 50,
+            bottom: 20,
+            left: 50
+        })
         .dimension(moveMonths)
         .group(volumeByMonthGroup)
         .centerBar(true)
         .gap(1)
-        .x(d3.time.scale().domain([new Date(2013, 0, 1), new Date(2014, 11, 31)]))
+        .x(d3.time.scale().domain([minDate, maxDate]))
         .round(d3.time.month.round)
         .alwaysUseRounding(true)
         .xUnits(d3.time.months);
@@ -257,7 +203,6 @@ d3.csv('../dist/BATBI-N-changed.csv', function (data) {
         .radius(80)
         .dimension(containerTypes)
         .group(containerTypesGroup)
-    // (_optional_) by default pie chart will use `group.key` as its label but you can overwrite it with a closure.
         .label(function (d) {
             if (containerTypesChart.hasFilter() && !containerTypesChart.hasFilter(d.key)) {
                 return d.key + '(0%)';
@@ -267,16 +212,98 @@ d3.csv('../dist/BATBI-N-changed.csv', function (data) {
                 label += '(' + Math.floor(d.value / all.value() * 100) + '%)';
             }
             return label;
-        })
-    
-    quarterChart /* dc.pieChart('#quarter-chart', 'chartGroup') */
+        });
+
+    shipmentTypesChart
+        .width(180)
+        .height(180)
+        .radius(80)
+        .dimension(shipmentTypes)
+        .group(shipmentTypesGroup)
+        .label(function (d) {
+            if (shipmentTypesChart.hasFilter() && !shipmentTypesChart.hasFilter(d.key)) {
+                return d.key + '(0%)';
+            }
+            var label = d.key;
+            if (all.value()) {
+                label += '(' + Math.floor(d.value / all.value() * 100) + '%)';
+            }
+            return label;
+        });
+
+    quarterChart
         .width(180)
         .height(180)
         .radius(80)
         .innerRadius(30)
         .dimension(quarter)
         .group(quarterGroup);
-    
+
+    originServicesChart
+        .width(180)
+        .height(180)
+        .margins({
+            top: 20,
+            left: 10,
+            right: 10,
+            bottom: 20
+        })
+        .group(originServicesGroup)
+        .dimension(originServices)
+        .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef'])
+        .label(function (d) {
+            return d.key;
+        })
+        .title(function (d) {
+            return d.value;
+        })
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+    destinationServicesChart
+        .width(180)
+        .height(180)
+        .margins({
+            top: 20,
+            left: 10,
+            right: 10,
+            bottom: 20
+        })
+        .group(destinationServicesGroup)
+        .dimension(destinationServices)
+        .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef'])
+        .label(function (d) {
+            return d.key;
+        })
+        .title(function (d) {
+            return d.value;
+        })
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+    dayOfWeekChart
+        .width(180)
+        .height(180)
+        .margins({
+            top: 20,
+            left: 10,
+            right: 10,
+            bottom: 20
+        })
+        .group(dayOfWeekGroup)
+        .dimension(dayOfWeek)
+        // Assign colors to each value in the x scale domain
+        .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb'])
+        .label(function (d) {
+            return d.key.split('.')[1];
+        })
+        // Title sets the row text
+        .title(function (d) {
+            return d.value;
+        })
+        .elasticX(true)
+        .xAxis().ticks(4);
+
     //#### Rendering
 
     //simply call `.renderAll()` to render all charts on the page
